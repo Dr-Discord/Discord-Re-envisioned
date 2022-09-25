e08c62ab81e9a95a3e4ba2d11f1df88427f92ff1
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2022-09-24 21:57:05 -0700

    Prompt api

--- src/main/modals.js
+++ src/main/modals.js
@@ -12,7 +12,10 @@ export default async (React) => {
   const ConfirmModal = await webpack.getModuleByDisplayNameAsync("ConfirmModal", true)
   
   const { Messages } = webpack.getAllModulesByProps("Messages")[1]
+
   const Button = webpack.getModuleByProps("ButtonColors", "ButtonSizes").default
+  const TextInput = webpack.getModuleByDisplayName("TextInput", true)
+  const TextArea = webpack.getModuleByDisplayName("TextArea", true)
 
   let renderMessageMarkup
 
@@ -28,6 +31,25 @@ export default async (React) => {
       content.shift()
       return React.createElement("div", { style: { color: "var(--text-normal)" } }, content)
     })() : con)
+  }
+
+  function Prompt({ danger, title, resolve, placeholder, defaultText, type, props }) {
+    const [ value, setValue ] = React.useState(defaultText)
+
+    return React.createElement(ConfirmModal, {
+      ...props,
+      header: title,
+      confirmText: Messages.OKAY,
+      onConfirm: () => resolve(value),
+      onCancel: () => resolve(null),
+      cancelText: Messages.CANCEL,
+      confirmButtonColor: danger ? Button.Colors.RED : Button.Colors.BRAND_NEW,
+      children: React.createElement(type, {
+        placeholder,
+        value,
+        onChange: (value) => setValue(value)
+      })
+    })
   }
 
   window.DrApi.modals = {
@@ -49,6 +71,26 @@ export default async (React) => {
         onConfirm,
         body: makeContent(content)
       }))
+    },
+    prompt: (title, opts = {}) => {
+      const { danger = false, placeholder = "", defaultText = "", large = false } = opts
+
+      const type = large ? TextArea : TextInput
+
+      return new Promise((resolve) => {
+        openModal(props => {
+          if (props.transitionState === 3) resolve(null)
+          return React.createElement(Prompt, {
+            props,
+            danger,
+            title,
+            resolve,
+            placeholder,
+            defaultText,
+            type
+          })
+        })
+      })
     },
     confirmModal: (title, content, opts = {}) => {
       const { cancelText = Messages.CANCEL, confirmText = Messages.OKAY, danger = false, onConfirm, onCancel } = opts
